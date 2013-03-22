d869a080b9024f1fe8514b92ba940762d0de473a
Lobo <lebebr01@gmail.com>
2013-03-22 12:11:26 -0500

    frequencies_to_r

--- R/DescriptivesToR.r
+++ R/DescriptivesToR.r
@@ -11,13 +11,15 @@ descriptives_to_r <- function(x){
   vars <- substr(x[varsLoc], (which(strsplit(x[varsLoc], '')[[1]]=='=')+1), nchar(x[varsLoc]))
   descVars <- paste(unlist(strsplit(gsub("^\\s+|\\s+$", "", vars), " ")), collapse = ", ")
   
-  statLoc <- grep("statistics\\s?=", x, ignore.case = TRUE)
+  statLoc <- grep("statistics\\s?=|stats\\s?=", x, ignore.case = TRUE)
   vars <- substr(x[statLoc], (which(strsplit(x[statLoc], '')[[1]]=='=')+1), nchar(x[statLoc]))
   vars <- gsub("^\\s+|\\s+$", "", gsub("\\.", "", vars))
   stats <- paste(unlist(strsplit(vars, " ")), collapse = ", ")
   stats <- tolower(stats)
   stats <- gsub("stddev", "sd", stats)
   stats <- gsub("variance", "var", stats)
+  stats <- gsub("minimum", "min", stats)
+  stats <- gsub("maximum", "max", stats)
   if(grepl("all", stats) == TRUE){
     stats <- paste("mean", "semean", "sd", "var", "kurtosis", "skewness", "range", "min", "max",
                    "sum", sep = ", ")
--- R/FrequenciesToR.r
+++ R/FrequenciesToR.r
@@ -9,24 +9,50 @@ frequencies_to_r <- function(x) {
   
   varsLoc <- grep("variables\\s?=", x, ignore.case = TRUE)
   vars <- substr(x[varsLoc], (which(strsplit(x[varsLoc], '')[[1]]=='=')+1), nchar(x[varsLoc]))
-  freqVars <- paste(unlist(strsplit(gsub("^\\s+|\\s+$", "", vars), " ")), collapse = ", ")
+  freqVars <- unlist(strsplit(gsub("^\\s+|\\s+$", "", vars), " "))
   
-  orderLoc <- grep("order\\s?=", x, ignore.case = TRUE)
+  #orderLoc <- grep("order\\s?=", x, ignore.case = TRUE)
   
-  formatLoc <- grep("format\\s?=", x, ignore.case = TRUE)
-  
-  if(grepl("\\/pie", x, ignore.case), == TRUE){
-    pie <- paste("ggplot(x, aes(x = factor(1), fill = ", freqVars, ")) + geom_bar() + 
-      coord_polar(theta = 'y')", sep = "")
+  if(any(grepl("format\\s?=\\s?notable", x, ignore.case = TRUE))){
+    freqOut <- ""
+  } else { 
+    freqOut <- sapply(1:length(freqVars), function(ii) 
+      paste("with(x, table(", freqVars[ii], "))", sep = '')) 
   }
   
-  if(grepl("\\/bar", x, ignore.case) == TRUE){
-    bar <- paste("ggplot(x, aes(x = factor(1), fill = ", freqVars, ")) + geom_bar()", sep = '')
-  }
+  if(any(grepl("^\\/stat", x, ignore.case = TRUE))){
+    statOut <- descriptives_to_r(x)
+  } else { statsOut <- "" }
   
-  if(grepl("\\/hist", x, ignore.case) == TRUE){
-    histogram <- paste("ggplot(x, aes(x = factor(1), fill = ", freqVars, ")) + geom_histogram()", 
-                       sep = '')
-  }
+  if(any(grepl("^\\/ntiles\\s?=", x, ignore.case = TRUE))){
+    ntileLoc <- grep("^\\/ntiles\\s?=", x, ignore.case = TRUE)
+    numBreak <- sapply(1:length(ntileLoc), function(ii) 
+      as.numeric(substr(x[ntileLoc[ii]], (which(strsplit(x[ntileLoc[ii]], '')[[1]]=='=')+1), 
+                                  nchar(x[ntileLoc[ii]]))))
+    ntilesOut <- sapply(1:length(ntileLoc), function(ii) 
+      paste("quantile(x, probs = seq(0, 1, 1/", numBreak[ii], "), type = 6)", sep = ''))
+  } else { ntilesOut <- "" }
+  
+  if(any(grepl("^\\/percentiles\\s?=", x, ignore.case = TRUE))){
+    perLoc <- grep("^\\/percentiles\\s?=", x, ignore.case = TRUE)
+    nums <- substr(x[perLoc], (which(strsplit(x[perLoc], '')[[1]]=='=')+1), nchar(x[perLoc]))
+    probNum <- paste(unlist(strsplit(gsub("^\\s+|\\s+$", "", nums), " ")), collapse = ", ")
+    percentileOut <- paste("quantile(x, probs = c(", probNum, "), type = 6)", sep = '') 
+  } else { percentileOut <- "" }
+  
+  if(any(grepl("^\\/pie", x, ignore.case = TRUE))){
+    pieG <- sapply(1:length(freqVars), function(ii) 
+      paste("ggplot(x, aes(x = factor(1), fill = ", freqVars[ii], ")) + geom_bar() + coord_polar(theta = 'y')", sep = ""))
+  } else { pieG <- "" }
+  
+  if(any(grepl("^\\/bar", x, ignore.case = TRUE))){
+    barG <- sapply(1:length(freqVars), function(ii) 
+      paste("ggplot(x, aes(x = factor(1), fill = ", freqVars[ii], ")) + geom_bar()", sep = ''))
+  } else { barG <- ""}
+  
+  if(any(grepl("^\\/hist", x, ignore.case = TRUE))){
+    histogramG <- sapply(1:length(freqVars), function(ii) 
+      paste("ggplot(x, aes(x = factor(1), fill = ", freqVars[ii], ")) + geom_histogram()", sep = ''))
+  } else { histogramG <- "" }
   
 }
