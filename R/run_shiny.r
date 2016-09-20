3d27c59ed1ca42112e4b217c86136417853b596a
Brandon LeBeau <lebebr01@gmail.com>
2016-09-20 16:00:29 -0500

    Add master shiny function.

---
+++ R/run_shiny.r
@@ -0,0 +1,17 @@
+#' Run Shiny Application Demo
+#' 
+#' Function runs Shiny Application Demo
+#' 
+#' This function does not take any arguments and will run the Shiny Application.
+#' If running from RStudio, will open the application in the viewer, otherwise will
+#' use the default internet browser.
+#' 
+#' @export
+run_shiny <- function() {
+  appDir <- system.file("shiny", package = "SPSStoR")
+  if (appDir == "") {
+    stop("Could not find example directory. Try re-installing `SPSStoR`.", call. = FALSE)
+  }
+  
+  shiny::runApp(appDir, display.mode = "normal")
+}
